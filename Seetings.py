00e00a81fb578a7d279c8d406d226bb055c74ba7
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2023-01-22 18:24:24 +0800

    Update Seetings.py

--- Seetings.py
+++ Seetings.py
@@ -1,5 +1,5 @@
-FONT_PATH = r"C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\方正少儿GBK简体.ttf"
+FONT_PATH = r".\方正少儿GBK简体.ttf"
 
-PVZ_IMAGE_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\base'
-PVZ_OUTPUT_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\output'
-PVZ_ORI_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\ori'
+PVZ_IMAGE_PATH = r'.\images\pvz\base'
+PVZ_OUTPUT_PATH = r'.\images\pvz\output'
+PVZ_ORI_PATH = r'.\images\pvz\ori'
