0777d63d5de57ae83a4fdaf4434b95f7944af187
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2023-01-22 21:25:19 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+with open("README.rst", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    setup(name='nonebot_plugin_pvz',  # 包名
+          version='1.0.0',  # 版本号
+          description='A plugin of nonebot2, which is support service to play pvz in group chat.',
+          long_description=long_description,
+          author='longchengguxiao',
+          author_email='1298919732@qq.com',
+          url='http://www.lcgx.space/home',
+          install_requires=[
+              "numpy",
+              "pillow",
+              "nonebot2"
+          ],
+          license='MIT License',
+          packages=find_packages(),
+          platforms=["all"],
+          classifiers=['Intended Audience :: Developers',
+                       'Operating System :: OS Independent',
+                       'Natural Language :: Chinese (Simplified)',
+                       'Programming Language :: Python',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Topic :: Software Development :: Libraries'
+                       ],
+          )
