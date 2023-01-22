a33e816ddb927695c8096a5964570e6d75788a62
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2023-01-22 18:03:59 +0800

    Update Seetings.py

--- Seetings.py
+++ Seetings.py
@@ -1,27 +1,5 @@
-TIANQIAPI_APPID='44691995'
-TIANQIAPI_APPSECRET='r6HF1aBM'
-TIANXING_API_APPKEY='3c0de0bd2df214b716fb180fba76253b'
-
-
-RESOURCE_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource'
-TXT_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\txt'
-IMAGE_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images'
-LOG_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\log'
 FONT_PATH = r"C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\方正少儿GBK简体.ttf"
-BLACKNAME_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\txt\blackname.txt'
-RECORD_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\record'
+
 PVZ_IMAGE_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\base'
 PVZ_OUTPUT_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\output'
 PVZ_ORI_PATH = r'C:\Users\Administrator\PycharmProjects\Yaobot\Yaobot\resource\images\pvz\ori'
-
-
-BAIDU_AK='C654GKIcAgsDdrtZL6l0RVXN0k51HikH'
-BAIDU_APPID='20210126000682312'
-BAIDU_KEY='RgzQhbKdsuj16VREV8Ie'
-
-
-
-
-
-STATE_OK = True
-STATE_ERROR = False
