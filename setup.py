ba9b4ee8965b43d4167df7e439e4bb0488aff1f3
longchengguxiao <75836227+longchengguxiao@users.noreply.github.com>
2023-01-22 23:46:53 +0800

    Create setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import find_packages
 with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
     setup(name='nonebot_plugin_pvz',  # 包名
-          version='1.0.2',  # 版本号
+          version='1.0.3',  # 版本号
           description='A plugin of nonebot2, which is support service to play pvz in group chat.',
           long_description=long_description,
           author='longchengguxiao',
           author_email='1298919732@qq.com',
           url='http://www.lcgx.space/home',
+          include_package_data=True,
           install_requires=[
               "numpy",
               "pillow",
